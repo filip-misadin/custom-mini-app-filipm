9fc4c95e3afd377655341bad48174becccb6d1a5
filip-misadin <filip.misadin@flabbergast.agency>
2025-04-01 12:02:27 +0200

    deeplink v6

--- src/components/ui/DeepLinkButton.jsx
+++ src/components/ui/DeepLinkButton.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 
 const DeepLinkButton = () => {
-  const handleClick = () => {
-    const androidLink =
-      "intent://worldcoin.org/mini-app?app_id=app_acd85595213ca97827cbadfa4bd85321#Intent;scheme=https;action=android.intent.action.VIEW;package=com.wordcoin;end;";
-    const iosLink =
-      "https://worldcoin.org/mini-app?app_id=app_acd85595213ca97827cbadfa4bd85321";
+  const androidLink =
+    "intent://worldcoin.org/mini-app?app_id=app_acd85595213ca97827cbadfa4bd85321#Intent;scheme=https;action=android.intent.action.VIEW;package=com.worldcoin;end;";
+  const iosLink =
+    "https://worldcoin.org/mini-app?app_id=app_acd85595213ca97827cbadfa4bd85321";
 
-    if (/Android/i.test(navigator.userAgent)) {
-      window.location.href = androidLink;
-    } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-      window.location.href = iosLink;
-    } else {
-      alert("This feature is only available on mobile devices.");
-    }
-  };
-
-  return <button onClick={handleClick}>Open in App</button>;
+  return (
+    <a
+      href={/Android/i.test(navigator.userAgent) ? androidLink : iosLink}
+      className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+    >
+      Open in App
+    </a>
+  );
 };
 
 export default DeepLinkButton;
